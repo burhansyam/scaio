2b20fc845cb91010329f761ce5613a552aca6616
Burhan Syam <23581412+burhansyam@users.noreply.github.com>
2022-11-28 03:56:10 +0700

    Update about.sh

--- commit/about.sh
+++ commit/about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m          ⇱ PapaVPN ⇲          \E[0m"
+echo -e "\E[44;1;39m          ⇱ Burhansyam ⇲          \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
